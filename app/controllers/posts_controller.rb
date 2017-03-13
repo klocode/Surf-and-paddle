7b0a62accbbf1087f646dfba39566863d4a17d71
klocode <kendrick.t.lo@gmail.com>
2017-03-13 14:34:01 -0400

    formatting navbar and fixed index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:edit, :show, :update]
+
   def index
     @posts = Post.order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
     @user = @post.user
     @popular_posts = Post.where("id != ?", params[:id]).order(created_at: :desc).limit(4)
     render 'home/index'
@@ -22,7 +23,22 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
 
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      params.require(:post).permit(:title, :body)
+    end
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user, counter_cache: true
+
+  validates :title, :body, :user, presence: true
 end
