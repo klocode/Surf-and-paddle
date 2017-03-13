c496cbeb7bab28b8e13889bffa2084eed3df5dc3
klocode <kendrick.t.lo@gmail.com>
2017-03-13 13:27:26 -0400

    created form for new and html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,24 @@ class PostsController < ApplicationController
     render 'home/index'
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to :root
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :body)
+  end
+
 end
