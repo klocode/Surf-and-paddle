f00656350efe672b2eab042ccb152d64628731a7
klocode <kendrick.t.lo@gmail.com>
2017-03-15 23:38:24 -0400

    added photo url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:user_id, :title, :body, :photo)
+    params.require(:post).permit(:user_id, :title, :body, :photo, :photo_url)
   end
 
   def is_owner
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,12 @@ class Post < ApplicationRecord
 
   mount_uploader :photo, PostPhotoUploader
 
-
+  validate :url_xor_photo
   validates :title, :body, :user, presence: true
-  validates_length_of :title, :minimum => 10
+  validates_length_of :title, {:minimum => 10, message: -> (object, chars) do
+                          "Your post title is #{(10 - chars[:value].length)} characters too short."
+                        end}
+
 
   default_scope { order(created_at: :desc) }
 
@@ -13,6 +16,24 @@ class Post < ApplicationRecord
 
   def is_owner?(this_user)
     user == this_user
+  end
+
+  # def image(version = :standard)
+  #   if photo?
+  #     photo.versions[version].url
+  #   elsif !picture_url.blank?
+  #     picture_url
+  #   else
+  #     "No image"
+  #   end
+
+
+
+  def url_xor_photo
+    if !photo? && photo_url.blank?
+      errors.add(:photo, "must exist if photo url doesn't")
+      errors.add(:photo_url, "must exist if photo doesn't")
+    end
   end
 
 end
